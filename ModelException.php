a1b51e04d105ab846223a4d9a40ef3a714023fa6
denis909 <denis909@mail.ru>
2019-10-18 18:29:34 +0300

    n/a

---
+++ ModelException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace denis909\yii;
+
+use yii\base\Model;
+
+class ModelException extends \yii\base\Exception
+{
+
+	public function __construct(Model $model)
+	{
+        $errors = $model->getFirstErrors();
+
+        $error = array_shift($errors);
+		
+		parent::__construct($error);
+	}
+
+}
